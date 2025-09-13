5445fb84ebd9f8bde47ea48d8af8ceab7152aeab
DJomblo <pklroy8@gmail.com>
2025-09-13 23:05:41 +0700

    Commit No. 1 – add fragment lifecyle logging

--- app/src/main/java/com/example/lab_week_03/MainFragment.kt
+++ app/src/main/java/com/example/lab_week_03/MainFragment.kt
@@ -5,6 +5,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.util.Log
+import android.content.Context
+
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
